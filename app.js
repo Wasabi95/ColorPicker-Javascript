cd06cd2832cd11ae65e759cdb534a55ce5639f45
Wasabi95 <carvajal.ups@gmail.com>
2022-03-14 17:16:11 -0400

    add project

---
+++ app.js
@@ -0,0 +1,13 @@
+let body = document.querySelector("body");
+document.querySelector("#red").onclick = function () {
+  body.style.backgroundColor = "red";
+};
+document.querySelector("#yellow").onclick = function () {
+  body.style.backgroundColor = "yellow";
+};
+document.querySelector("#crimson").onclick = function () {
+  body.style.backgroundColor = "crimson";
+};
+document.querySelector("#blue").onclick = function () {
+  body.style.backgroundColor = "blue";
+};
